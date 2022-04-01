f2482755f983e53f2bc730e898e6a72dbac06691
Bevz Aleksandr <as-bivz@yandex.ru>
2022-04-01 16:34:00 +0300

    Update a game, but bugs...

--- src/main/java/ru/bevz/aj/generics/game/Team.java
+++ src/main/java/ru/bevz/aj/generics/game/Team.java
@@ -4,9 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-public class Team {
+public class Team <T>{
     private String name;
-    private List<Player> players = new ArrayList<>();
+    private List<T> players = new ArrayList<>();
 
     public Team(String name) {
         this.name = name;
@@ -20,20 +20,20 @@ public class Team {
         this.name = name;
     }
 
-    public List<Player> getPlayers() {
+    public List<T> getPlayers() {
         return players;
     }
 
-    public void setPlayers(List<Player> players) {
+    public void setPlayers(List<T> players) {
         this.players = players;
     }
 
-    public void addPlayer(Player player) {
+    public void addPlayer(T player) {
         players.add(player);
-        System.out.println("Team update: add a new player: " + player.getName());
+        System.out.println("Team update: add a new player: " + ((Player) player).getName());
     }
 
-    public void playWith(Team team) {
+    public void playWith(Team<T> team) {
         Random random = new Random();
         int i = random.nextInt(2);
         String winner = i == 0 ? this.getName() : team.getName();
--- src/main/java/ru/bevz/aj/generics/game/Test.java
+++ src/main/java/ru/bevz/aj/generics/game/Test.java
@@ -2,18 +2,27 @@ package ru.bevz.aj.generics.game;
 
 public class Test {
     public static void main(String[] args) {
-        Player pupil1 = new Pupil("Bob", 13);
-        Player pupil2 = new Pupil("Ivan", 14);
+        Pupil pupil1 = new Pupil("Bob", 13);
+        Pupil pupil2 = new Pupil("Ivan", 14);
 
-        Player student1 = new Student("Keks", 20);
-        Player student2 = new Student("Borya", 21);
+        Student student1 = new Student("Keks", 20);
+        Student student2 = new Student("Borya", 21);
 
-        Player employee1 = new Employee("Bruuuh", 26);
-        Player employee2 = new Employee("Noise", 28);
+        Employee employee1 = new Employee("Bruuuh", 26);
+        Employee employee2 = new Employee("Noise", 28);
 
-        Team pupilTeam = new Team("MilkSuckers");
+        Team<Pupil> pupilTeam = new Team<>("MilkSuckers");
         pupilTeam.addPlayer(pupil1);
         pupilTeam.addPlayer(pupil2);
 
+        Team<Student> studentTeam = new Team<>("BeerSuckers");
+        studentTeam.addPlayer(student1);
+        studentTeam.addPlayer(student2);
+
+        Team<Employee> employeeTeam = new Team<>("Boomers");
+        employeeTeam.addPlayer(employee1);
+        employeeTeam.addPlayer(employee2);
+
+        Team<String> anotherTeam = new Team<>("Bruuuh, It's a bug!");
     }
 }
