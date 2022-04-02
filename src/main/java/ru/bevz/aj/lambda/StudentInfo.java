fa0c7ad5aa8d16a8f3d82f5668e638f9d29a2c40
Bevz Aleksandr <as-bivz@yandex.ru>
2022-04-02 11:16:30 +0300

    Update class StudentInfo
    Implemented by interface.

--- src/main/java/ru/bevz/aj/lambda/StudentInfo.java
+++ src/main/java/ru/bevz/aj/lambda/StudentInfo.java
@@ -3,6 +3,15 @@ package ru.bevz.aj.lambda;
 import java.util.ArrayList;
 
 public class StudentInfo {
+
+    void testStudents(ArrayList<Student> al,StudentChecks sc) {
+        for (Student s : al) {
+            if (sc.check(s)) {
+                System.out.println(s);
+            }
+        }
+    }
+
     public static void main(String[] args) {
         Student st1 = new Student("Ivan", 'm', 22, 5, 8.3);
         Student st2 = new Student("Volya", 'm', 20, 2, 6.3);
@@ -21,39 +30,52 @@ public class StudentInfo {
         students.add(st6);
 
         StudentInfo info = new StudentInfo();
-        info.printStudentOverGrade(students, 8.0);
-        printDash();
-        info.printStudentsUnderAge(students, 20);
-        printDash();
-        info.printStudentMixCondition(students, 8.5, 20, 'm');
+        info.testStudents(students, new CheckOverGrade());
+//        info.printStudentOverGrade(students, 8.0);
+//        printDash();
+//        info.printStudentsUnderAge(students, 20);
+//        printDash();
+//        info.printStudentMixCondition(students, 8.5, 20, 'm');
 
     }
 
-    static void printDash() {
-        System.out.println("------------------------------------");
-    }
+//    static void printDash() {
+//        System.out.println("------------------------------------");
+//    }
+//
+//    void printStudentOverGrade(ArrayList<Student> al, double grade) {
+//        for (Student s : al) {
+//            if (s.getAvgGrade() < grade) {
+//                System.out.println(s);
+//            }
+//        }
+//    }
+//
+//    void printStudentsUnderAge(ArrayList<Student> al, int age) {
+//        for (Student s : al) {
+//            if (s.getAge() < age) {
+//                System.out.println(s);
+//            }
+//        }
+//    }
+//
+//    void printStudentMixCondition(ArrayList<Student> al, double grade, int age, char sex) {
+//        for (Student s : al) {
+//            if (s.getAge() > age && s.getAvgGrade() < grade && s.getSex() == sex) {
+//                System.out.println(s);
+//            }
+//        }
+//    }
+}
 
-    void printStudentOverGrade(ArrayList<Student> al, double grade) {
-        for (Student s : al) {
-            if (s.getAvgGrade() < grade) {
-                System.out.println(s);
-            }
-        }
-    }
+interface StudentChecks {
+    boolean check(Student s);
+}
 
-    void printStudentsUnderAge(ArrayList<Student> al, int age) {
-        for (Student s : al) {
-            if (s.getAge() < age) {
-                System.out.println(s);
-            }
-        }
-    }
+class CheckOverGrade implements StudentChecks {
 
-    void printStudentMixCondition(ArrayList<Student> al, double grade, int age, char sex) {
-        for (Student s : al) {
-            if (s.getAge() > age && s.getAvgGrade() < grade && s.getSex() == sex) {
-                System.out.println(s);
-            }
-        }
+    @Override
+    public boolean check(Student s) {
+        return s.avgGrade > 8.0;
     }
 }
