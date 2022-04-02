080e1c05f9f544e491bbf1d5c7a2965eb9190273
Bevz Aleksandr <as-bivz@yandex.ru>
2022-04-02 13:15:13 +0300

    Add class
    Demonstrated the interface Supplier

---
+++ src/main/java/ru/bevz/aj/lambda/Test3.java
@@ -0,0 +1,77 @@
+package ru.bevz.aj.lambda;
+
+import java.util.ArrayList;
+import java.util.function.Supplier;
+import java.util.function.Consumer;
+
+
+public class Test3 {
+
+    public static ArrayList<Car> createThreeCars(Supplier<Car> carSupplier) {
+        ArrayList<Car> al = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            al.add(carSupplier.get());
+        }
+        return al;
+    }
+
+    public static void changeCar(Car car, Consumer<Car> carConsumer) {
+        carConsumer.accept(car);
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Car> ourCars = createThreeCars(() ->
+                new Car("Nissan Tilda", "Silver Metalic", 1.6)
+        );
+
+        System.out.println("Our cars: " + ourCars);
+
+        changeCar(ourCars.get(0), car -> car.color = "Black Nigger");
+        System.out.println(ourCars.get(0));
+    }
+}
+
+class Car {
+    String model;
+    String color;
+    double engine;
+
+    public Car(String model, String color, double engine) {
+        this.model = model;
+        this.color = color;
+        this.engine = engine;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "model='" + model + '\'' +
+                ", color='" + color + '\'' +
+                ", engine=" + engine +
+                '}';
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double getEngine() {
+        return engine;
+    }
+
+    public void setEngine(double engine) {
+        this.engine = engine;
+    }
+}
