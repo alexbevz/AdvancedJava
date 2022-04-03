77e815c8a8bb19a64cc8fd17b528133a2e0428d4
Bevz Aleksandr <as-bivz@yandex.ru>
2022-04-03 11:02:10 +0300

    Add class with the nested classes

---
+++ src/main/java/ru/bevz/aj/nested_classes/Test.java
@@ -0,0 +1,19 @@
+package ru.bevz.aj.nested_classes;
+
+public class Test {
+
+    public static class A {
+
+    }
+
+    public class B {
+
+    }
+
+    public void method() {
+        class C {
+
+        }
+    }
+
+}
